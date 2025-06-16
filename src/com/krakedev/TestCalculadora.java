267948b585e6e6d1da87aedcd2285fdc1f23baaa
FranklinW2002 <franklinmunoz2002@gmail.com>
2025-06-15 22:51:52 -0500

    se probaron los metodos desde TestCalculadora

--- src/com/krakedev/TestCalculadora.java
+++ src/com/krakedev/TestCalculadora.java
@@ -7,6 +7,9 @@ public class TestCalculadora {
 		calcu = new Calculadora();
 		int resultadoSuma;
 		int resultadoResta;
+		double resultadoMulti;
+		double resultadoDiv;
+		double promedio;
 		
 		resultadoSuma = calcu.sumar(5,3);
 		
@@ -15,7 +18,20 @@ public class TestCalculadora {
 		resultadoResta = calcu.restar(10,3);
 		
 		System.out.println("Resultado: " + resultadoResta);
-
+		
+		resultadoMulti = calcu.mutiplicar(2.5, 3.8);
+		
+		System.out.println("Resultado: " + resultadoMulti);
+		
+		resultadoDiv = calcu.dividir(9.5, 2.2);
+		
+		System.out.println("Resultado: " + resultadoDiv);
+		
+		promedio = calcu.promediar(9, 8.5, 7.5);
+		
+		System.out.println("Resultado: " + promedio);
+		
+		calcu.mostraResulado();
+		
 	}
-
 }
