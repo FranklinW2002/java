84991179ea269e52cb078e19fa3bf5f7a3529cb8
FranklinW2002 <franklinmunoz2002@gmail.com>
2025-06-15 22:46:22 -0500

    se agrego el metodo dividir

--- src/com/krakedev/Calculadora.java
+++ src/com/krakedev/Calculadora.java
@@ -26,5 +26,14 @@ public class Calculadora {
 		return resultado;
 	}
 	
-
+	public double dividir(double valor1, double valor2) {
+		
+		double resultado;
+		
+		resultado = valor1 / valor2;
+		
+		return resultado;
+	}
+	
+	
 }
